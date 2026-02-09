74329c8af97d3d4cce3ed3cc3abe06a097313262
ididit374-lgtm <Ididit374@gmail.com>
2026-02-09 12:06:19 +0000

    Temporarily remove Procfile

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "postgresql://neondb_owner:npg_sklLDg2oJ5qc@ep-dry-sky-agl8fkpv.c-2.eu-central-1.aws.neon.tech/most_land_elf_662242", "")
+os.environ.setdefault("SECRET_KEY", "")
